2fd29028cb99b163e228ed806b85f9f68d1f9c25
Ashley Huo <x2huo@uwaterloo.ca>
2024-06-07 17:20:45 -0400

    basic personal website structure

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+const Blog = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>Welcome to my blog. Here are some posts.</p>
+        </div>
+    );
+};
+
+export default Blog;
