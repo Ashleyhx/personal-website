d745086ef734690c98b45dfd9cb28c74ab314908
Ashley Huo <x2huo@uwaterloo.ca>
2024-06-07 19:02:03 -0400

    formmating

--- app/components/WebGLCanvas.tsx
+++ app/components/WebGLCanvas.tsx
@@ -53,7 +53,7 @@ const WebGLCanvas = () => {
         };
     }, []);
 
-    return <div ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100vh', zIndex: -1 }} />;
+    return <div ref={canvasRef} className="webgl-canvas" />;
 };
 
 export default WebGLCanvas;
