e69184594961058acd86ece48fa17e10cfa0fcd4
Ashley Huo <x2huo@uwaterloo.ca>
2024-06-08 00:02:20 -0400

    # This is a combination of 8 commits. # This is the 1st commit message:
    Configure Next.js for static export and update GitHub Actions workflow
    
    # This is the commit message #2:
    
    fix
    
    # This is the commit message #3:
    
    fix config
    
    # This is the commit message #4:
    
    fix
    
    # This is the commit message #5:
    
    fix
    
    # This is the commit message #6:
    
    fix artifact
    
    # This is the commit message #7:
    
    remove next export
    
    # This is the commit message #8:
    
    try fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    // assetPrefix: './', // Ensures assets are correctly referenced
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
